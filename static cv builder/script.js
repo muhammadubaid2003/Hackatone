42109be17e6bd221c4c540e265215a9ab7f7fff0
Muhammad Ubaid <93512987+muhammadubaid2003@users.noreply.github.com>
2024-09-08 04:34:25 +0500

    Delete static cv builder directory

--- static cv builder/script.js
+++
@@ -1,7 +0,0 @@
-var toggleSkillsButton = document.getElementById('toggle-skills');
-var skillsSection = document.getElementById('skills');
-toggleSkillsButton === null || toggleSkillsButton === void 0 ? void 0 : toggleSkillsButton.addEventListener('click', function () {
-    if (skillsSection) {
-        skillsSection.style.display = skillsSection.style.display === 'none' ? 'block' : 'none';
-    }
-});
--- static cv builder/script.ts
+++
@@ -1,8 +0,0 @@
-const toggleSkillsButton = document.getElementById('toggle-skills');
-const skillsSection = document.getElementById('skills');
-
-toggleSkillsButton?.addEventListener('click', () => {
-    if (skillsSection) {
-        skillsSection.style.display = skillsSection.style.display === 'none' ? 'block' : 'none';
-    }
-});
